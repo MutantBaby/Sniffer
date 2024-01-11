3debccca0833ec3bdfa6b1a500b2ed244b5d6b32
MutantBaby <shaheryarbakir998877@gmail.com>
2024-01-11 09:57:56 +0500

    impl scan fn

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use core::num;
 use std::{
     env::{self},
-    net::IpAddr,
+    io::{self, Write},
+    net::{IpAddr, TcpStream},
     process,
     str::FromStr,
+    sync::mpsc::{channel, Sender},
+    thread,
 };
+
+const MAX: u16 = 65535;
 
 struct Cli {
     flag: String,
@@ -61,6 +67,26 @@ impl Cli {
     }
 }
 
+fn scan(x_transmitter: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                x_transmitter.send(port).unwrap();
+            }
+            Err(_) => {}
+        };
+
+        if (MAX - port) <= num_threads {
+            break;
+        }
+
+        port += num_threads;
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program: String = args[0].clone();
@@ -73,6 +99,17 @@ fn main() {
             process::exit(0);
         }
     });
+
+    let num_threads = arguments.threads;
+    let (x_transmitter, x_receiver) = channel();
+
+    for i in 0..num_threads {
+        let x_transmitter = x_transmitter.clone();
+
+        thread::spawn(move || {
+            scan(x_transmitter, i, arguments.ipaddr, num_threads);
+        });
+    }
 
     println!("Args {:?}", args);
 }
