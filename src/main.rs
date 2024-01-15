4b935f21311bbb560b68f01acfd4356bcef5a834
MutantBaby <shaheryarbakir998877@gmail.com>
2024-01-15 10:11:32 +0500

    refc code & deps

--- src/main.rs
+++ src/main.rs
@@ -44,62 +44,43 @@ fn end_port_guard(input: &u16) -> bool {
     *input <= 65535
 }
 
-// fn scan(x_transmitter: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
-//     let mut port = start_port + 1;
-//     loop {
-//         match TcpStream::connect((addr, port)) {
-//             Ok(_) => {
-//                 print!(".");
-//                 io::stdout().flush().unwrap();
-//                 x_transmitter.send(port).unwrap();
-//             }
-//             Err(_) => {}
-//         };
+async fn scan(x_transmitter: Sender<u16>, port: u16, addr: IpAddr) {
+    match TcpStream::connect(format!("{}:{}", addr, port)).await {
+        Ok(_) => {
+            print!(".");
+            io::stdout().flush().unwrap();
+            x_transmitter.send(port).unwrap();
+        }
+        Err(_) => {}
+    };
+}
 
-//         if (MAX - port) <= num_threads {
-//             break;
-//         }
+#[tokio::main]
+async fn main() {
+    let opts: Cli = cli().run();
 
-//         port += num_threads;
-//     }
-// }
+    let (x_transmitter, x_receiver) = channel();
 
-fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // let program: String = args[0].clone();
+    for i in opts.start_port..opts.end_port {
+        let x_transmitter = x_transmitter.clone();
 
-    // let arguments: Cli = Cli::new(&args).unwrap_or_else(|err| {
-    //     if err.contains("help") {
-    //         process::exit(0);
-    //     } else {
-    //         eprintln!("{} problem parsing arguments: {}", program, err);
-    //         process::exit(0);
-    //     }
-    // });
+        task::spawn(async move {
+            scan(x_transmitter, i, opts.address).await;
+        });
+    }
 
-    // let num_threads = arguments.threads;
-    // let (x_transmitter, x_receiver) = channel();
+    let mut out: Vec<u16> = vec![];
+    drop(x_transmitter);
 
-    // for i in 0..num_threads {
-    //     let x_transmitter = x_transmitter.clone();
+    for p in x_receiver {
+        out.push(p);
+    }
 
-    //     thread::spawn(move || {
-    //         scan(x_transmitter, i, arguments.ipaddr, num_threads);
-    //     });
-    // }
+    println!(" ");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
 
-    // let mut out = vec![];
-    // drop(x_transmitter);
-
-    // for p in x_receiver {
-    //     out.push(p);
-    // }
-
-    // println!(" ");
-    // out.sort();
-    // for v in out {
-    //     println!("{} is open", v);
-    // }
-
-    // println!("Args {:?}", args);
+    println!("Args {:?}", opts);
 }
